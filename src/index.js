57ba123d26dfef3d0c811caf7c57c411437d79df
Julian Phillips <julian.p@me.com>
2019-11-22 15:36:47 -0500

    fix: Added imports for react bootstrap, fixed App.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
 import { HashRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <HashRouter>
