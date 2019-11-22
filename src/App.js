8c511beb2da7e33114d2c7bcb2685187870baa92
Julian Phillips <julian.p@me.com>
2019-11-22 14:54:33 -0500

    feat: Added rough styling to the navbar, merge conflicts.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
+import Navbar from "./Components/Navbar/Navbar";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Craigslist Mock Up</h1>
     </div>
   );
