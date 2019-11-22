c8f10ea803c2f6ba653403f8a3d9236d50e20f7a
Julian Phillips <julian.p@me.com>
2019-11-22 14:46:02 -0500

    feat: Added mock items to nav.

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Navbar.css";
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>Logo will be here</h1>
+      <button></button>
+      <button></button>
+      <button></button>
+      <button></button>
+      <h1>User Icon will be here</h1>
+    </div>
+  );
+}
