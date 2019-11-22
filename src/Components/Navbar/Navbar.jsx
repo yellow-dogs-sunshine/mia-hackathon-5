070aa21fc5a7d5b24286fd3221296b635d55fa20
Julian Phillips <julian.p@me.com>
2019-11-22 14:52:59 -0500

    feat: Added rough styling to the navbar.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,10 +5,12 @@ export default function Navbar() {
   return (
     <div className="navbar-container">
       <h1>Logo will be here</h1>
-      <button></button>
-      <button></button>
-      <button></button>
-      <button></button>
+      <div className="crumbs-container">
+        <button></button>
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
       <h1>User Icon will be here</h1>
     </div>
   );
