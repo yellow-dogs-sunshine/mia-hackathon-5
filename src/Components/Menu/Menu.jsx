7373562fd87bdb958b753ded6ad684019033b961
Julian Phillips <julian.p@me.com>
2019-11-22 15:45:05 -0500

    feat: Replaced buttons with Breadcrumbs.

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-import { Navbar } from "react-bootstrap";
+import { Breadcrumb, Navbar } from "react-bootstrap";
 import "./Menu.css";
 export default function Menu() {
   return (
     <Navbar className="navbar-container">
       <p className="logo">CL</p>
-      <div className="crumbs-container">
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-      </div>
+
+      <Breadcrumb>
+        <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+        <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+          Library
+        </Breadcrumb.Item>
+        <Breadcrumb.Item active>Data</Breadcrumb.Item>
+      </Breadcrumb>
+
       <p>User Icon will be here</p>
     </Navbar>
   );
