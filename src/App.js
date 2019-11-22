2e731244537d4c9f50a2a09c69a9a5c861605988
kikibres <kristinabressler@yahoo.com>
2019-11-22 15:26:59 -0500

    gallery added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { Component }  from "react";
 import "./App.css";
-import Navbar from './components/Navbar';
-import Cardjson from '../public/craigslist.json';
+// import Navbar from './components/Navbar';
+import Cardjson from './craigslist.json';
+import Card from './Components/Card/card';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        list: Cardjson
+    }
+}
+showCard = () =>{
+  return this.state.list.map((eachCard,i)=>{
+      return(
+        <Card className="card" key={i} imgs={eachCard["Image Links "]}></Card>
+      )
+  })
+}
+render () {
+  console.log("this is list", this.state.list);
   return (
     <div className="App">
       <h1>Craigslist Mock Up</h1>
-      <Navbar></Navbar>
+      {/* <Navbar></Navbar> */}
+      <div className="gallery">
+      {this.showCard()}
+      </div>
     </div>
   );
+}
 }
 
 export default App;
--- src/Components/Card/card.js
+++ src/Components/Card/card.js
@@ -1,10 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component }  from "react";
+import './card.css';
+// import Cardjson from './craigslist.json.js';
 
 export class card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPic: 0,
+      slidesArr: this.props.imgs
+      // list: Cardjson
+    }
+}
+clickLeft = () => {
+  let moveleftArr = [...this.state.slidesArr]
+  const lastIndex = moveleftArr.length - 1;
+  const { currentPic } = this.state;
+  const shouldResetIndex = currentPic === 0;
+  const index =  shouldResetIndex ? lastIndex : currentPic - 1;
+
+  this.setState({
+    currentPic: index
+  });
+}
+
+clickRight = () => {
+  let moverightArr = [...this.state.slidesArr]
+  const lastIndex = moverightArr.length - 1;
+  const { currentPic } = this.state;
+  const shouldResetIndex = currentPic === lastIndex;
+  const index =  shouldResetIndex ? 0 : currentPic + 1;
+
+  this.setState({
+    currentPic: index
+  });
+}
   render() {
     return (
-      <div>
-        
+      <div className="card-wrapper">
+        <div className="carousel">
+        <button onClick={this.clickLeft}>Left</button>
+        <img src={this.state.slidesArr[this.state.currentPic]}  alt="carousel" />
+        <button onClick={this.clickRight}>Right</button>
+        </div>
+        <div className="content">
+          <h3>Details</h3>
+          <h4>Price</h4>
+        </div>
       </div>
     )
   }
